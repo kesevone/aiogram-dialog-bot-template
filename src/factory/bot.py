8ef991fbb41c686461336ba4c6b535a17a784522
kesevone <70077088+kesevone@users.noreply.github.com>
2024-06-28 17:05:21 +0300

    Set ParseMode to Optional, default > HTML

--- src/factory/bot.py
+++ src/factory/bot.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Optional
 
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
@@ -10,7 +11,7 @@ from src.app_config import AppConfig
 from src.utils import msgspec_json as mjson
 
 
-def create_bot(config: AppConfig, parse_mode: ParseMode) -> Bot:
+def create_bot(config: AppConfig, parse_mode: Optional[ParseMode] = ParseMode.HTML) -> Bot:
     session: AiohttpSession = AiohttpSession(
         json_loads=mjson.decode, json_dumps=mjson.encode
     )
